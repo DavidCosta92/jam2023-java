40dbaab9afba954acbeaf4a3c1c3cf962091db98
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2024-08-20 09:42:05 -0300

    modificacion de base de datos

--- src/main/java/com/example/JAM23/demo/DemoApplication.java
+++ src/main/java/com/example/JAM23/demo/DemoApplication.java
@@ -10,26 +10,31 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args);}
-
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
 	@Bean
-	public OpenAPI swaggerDocsConfig (){
+	public OpenAPI swaggerDocsConfig() {
 		return new OpenAPI()
 				.info(new Info()
 						.title("JAM 2023 - Academia")
 						.version("1.0")
-						.description("Aplicación web, para la gestión de inscripciones, asistencias y usuarios académicos, como parte del evento JAM 2023 para crear soluciones de software gratuitas para emprendedores. A partir de una problemática propuesta, se analizó posibles soluciones que llevaron a diseñar e implementar un MVP, usando Java, Spring, Spring security, MySQL, PostgreSQL, React Js, Swagger, Figma, Github, Notion, etc.\n" +
-								"\n" +
-								"Aplicacion desplegada en Railway\n" +
-								"\n" +
-								"Repositorio => https://github.com/DavidCosta92/jam2023-java\n" +
-								"\n" +
-								"Documentacion => https://tense-receipt-production.up.railway.app/docs/swagger-ui/index.html\n" +
-								"\n" +
-								"Deploy de Api => https://tense-receipt-production.up.railway.app/course/")
-						.summary("Deploy: https://tense-receipt-production.up.railway.app/course/")
-						.contact(new Contact().email("davidcst2991@gmail.com").name("Costa david").url("https://www.linkedin.com/in/david-costa-yafar/")));
+						.description(
+								"Aplicación web, para la gestión de inscripciones, asistencias y usuarios académicos, como parte del evento JAM 2023 para crear soluciones de software gratuitas para emprendedores. A partir de una problemática propuesta, se analizó posibles soluciones que llevaron a diseñar e implementar un MVP, usando Java, Spring, Spring security, MySQL, PostgreSQL, React Js, Swagger, Figma, Github, Notion, etc.\n"
+										+
+										"\n" +
+										"Aplicacion desplegada en Railway (Backend) y clever cloud (Base de datos).\n" +
+										"\n" +
+										"Repositorio => https://github.com/DavidCosta92/jam2023-java\n" +
+										"\n" +
+										"Documentacion => https://jam2023-java-production.up.railway.app/docs/swagger-ui/index.html\n"
+										+
+										"\n" +
+										"Deploy de Api => https://jam2023-java-production.up.railway.app/course/")
+						.summary("Deploy: https://jam2023-java-production.up.railway.app/course/")
+						.contact(new Contact().email("davidcst2991@gmail.com").name("Costa david")
+								.url("https://www.linkedin.com/in/david-costa-yafar/")));
 	}
 
 }
--- src/main/java/com/example/JAM23/demo/config/SecurityConfig.java
+++ src/main/java/com/example/JAM23/demo/config/SecurityConfig.java
@@ -10,9 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -29,7 +31,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(csrf-> csrf.disable()).headers().disable()
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 //.headers().httpStrictTransportSecurity().maxAgeInSeconds(0).includeSubDomains(true)
                 //.headers().httpStrictTransportSecurity().disable()
                 .authorizeHttpRequests(auth -> auth
